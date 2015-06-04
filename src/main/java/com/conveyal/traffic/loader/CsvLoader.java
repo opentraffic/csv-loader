8e2beb608792fbcdaf0950efef190ff0c098af42
Matthew Wigginton Conway <matt@indicatrix.org>
2015-06-04 17:53:03 -0400

    fix build.

--- src/main/java/com/conveyal/traffic/loader/CsvLoader.java
+++ src/main/java/com/conveyal/traffic/loader/CsvLoader.java
@@ -1,17 +1,19 @@
 package com.conveyal.traffic.loader;
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.conveyal.traffic.data.ExchangeFormat;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.*;
 import java.nio.charset.Charset;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -22,54 +24,44 @@ import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.Options;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import com.conveyal.traffic.data.ExchangeFormat;
-
 public class CsvLoader {
 	
-	public static void main(String[] args) throws org.apache.commons.cli.ParseException, IOException {
-		
-		Options options = new Options();
-		
-		options.addOption("f", true, "specify CSV file (can be compressed as .csv.gz or csv.zip)");
-		
-		options.addOption("u", true, "specify URL for traffic engine file (defauls to 'http://localhost/')");
-		
-		CommandLineParser parser = new DefaultParser();
-		CommandLine cmd = parser.parse(options, args);
-		
-		String urlStr = "http://localhost/";
-		
-		if(cmd.hasOption("u"))
-			urlStr = cmd.getOptionValue("u");
-		
-		if(!cmd.hasOption("f")) {
-			System.out.println("CSV input file not specified");
-			return;
-		}
-		
-		System.out.println(cmd.getOptionValue("f"));
-		
-		File csvFile = new File(cmd.getOptionValue("f"));
-		
-		if(!csvFile.exists()) {
-			System.out.println("CSV input file does not exist: " + csvFile.getAbsolutePath());
-			return;
-		}
-		else {
-			long sourceId = UUID.randomUUID().getLeastSignificantBits();
-			processCsv(sourceId, csvFile, urlStr);
+	public static void main(String[] args) {
+		try {
+			Options options = new Options();
+
+			options.addOption("f", true,
+					"specify CSV file (can be compressed as .csv.gz or csv.zip)");
+
+			options.addOption("u", true,
+					"specify URL for traffic engine file (defauls to 'http://localhost/')");
+
+			CommandLineParser parser = new DefaultParser();
+			CommandLine cmd = parser.parse(options, args);
+
+			String urlStr = "http://localhost:9000/";
+
+			if (cmd.hasOption("u"))
+				urlStr = cmd.getOptionValue("u");
+
+			if (!cmd.hasOption("f")) {
+				System.out.println("CSV input file not specified");
+				return;
+			}
+
+			System.out.println(cmd.getOptionValue("f"));
+
+			File csvFile = new File(cmd.getOptionValue("f"));
+
+			if (!csvFile.exists()) {
+				System.out.println("CSV input file does not exist: " + csvFile.getAbsolutePath());
+				return;
+			} else {
+				long sourceId = UUID.randomUUID().getLeastSignificantBits();
+				processCsv(sourceId, csvFile, urlStr);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 			
 	}
@@ -103,9 +95,11 @@ public class CsvLoader {
 				fileStream = new FileInputStream(csvFile);
 				decoder = new InputStreamReader(fileStream, Charset.forName("UTF-8"));
 			}
-				 
-			
+
 			parser = new CSVParser(decoder, CSVFormat.RFC4180);
+
+			CloseableHttpClient client = HttpClients.createDefault();
+
 			for (CSVRecord csvRecord : parser.getRecords()) {
 
 				String timeStr = csvRecord.get(0);
@@ -143,9 +137,8 @@ public class CsvLoader {
 				HttpPost httpPost = new HttpPost(url);
 				ByteArrayEntity entity = new ByteArrayEntity(postData);
 				httpPost.setEntity(entity);
-				HttpClient client = new DefaultHttpClient();
 				client.execute(httpPost);
-				
+				httpPost.releaseConnection();
 			}
 			
 			parser.close();
